bf7fd1f111968a863df1a23909fa044e0de305a8
Djambek <d.d.tlepbergenov@gmail.com>
2023-11-22 00:33:25 +0300

    code review

--- C1.py
+++ C1.py
@@ -1,10 +1,10 @@
 import ParserMPUtimetable
 
 def giveLink(group):
+    """Это функция предназначена для возврата ссылки на файл ics для даннной группы"""
     link = (0, ) # getLinkDB(group)
     if link[0] == 0:
         link = ParserMPUtimetable.timetableParser(group)
-        # print("link is", link)
         if link[0] == 4: return 1
         elif link[0] == 2 or link[0] == 3 or link[0] == 5: return (2,)
         elif link[0] == 6: return (3,)
