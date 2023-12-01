a6ab460674ff8eed569851370e93c44521294df5
Varvara Khomenko <varvara.khomenko.0@gmail.com>
2023-12-01 18:56:15 +0300

    Add gitingore

--- UpdateSchedule.py
+++ UpdateSchedule.py
@@ -2,8 +2,8 @@ from ParserMPUtimetable import *
 from DBMPU import *
 if get_hashgroup()[0] == 4:
     for value in get_hashgroup()[1]:
-        group = value[1][0]
-        hashh = value[1][1]
-        json_data = getJSON()
+        group = value[0]
+        hashh = value[1]
+        json_data = getJSON(group)
         if json_data[2] != hashh:
             lessons_schedule(json_data, group)
