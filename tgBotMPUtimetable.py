5a89bbc02df80a9bdc7b3172fd74184514821893
Djambek <44769905+Djambek@users.noreply.github.com>
2023-12-04 21:57:27 +0300

    Update tgBotMPUtimetable.py

--- tgBotMPUtimetable.py
+++ tgBotMPUtimetable.py
@@ -6,7 +6,7 @@ token='6406936841:AAE0K6Tn7MHNDHRkexecSB4a-66dpTNRoHQ'
 bot=telebot.TeleBot(token)
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id,'Привет! Чтобы получить ссылку, отправьте команду /get_timetable')
+    bot.send_message(message.chat.id,'Привет! Чтобы получить расписание, отправьте команду /get_timetable')
     print(message.chat.id)
 
 @bot.message_handler(commands=['get_timetable'])
@@ -31,6 +31,7 @@ def Group(message):
          bot.send_message(message.chat.id,'Введенной вами группы не существует, проверьте название группы на сайте политеха' )
     else:
         bot.send_message(message.chat.id,'Произошла ошибка при получении ссылки, попробуйте еще раз или обратитесь в поддержку ')
+    bot.send_document(message.chat.id,"В случае возникновения технических шоколадок писать @VarvaraKh")
 
 
 
